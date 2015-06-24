2a005d6a1419f0029dd5fda6abb6e5b06a66d066
Esa Varemo <esa@kuivanto.fi>
2015-06-24 17:50:58 +0300

    Fix an error created by cleaning up

--- app/src/main/java/fi/dy/esav/icingaandroidclient/IcingaWidget.java
+++ app/src/main/java/fi/dy/esav/icingaandroidclient/IcingaWidget.java
@@ -19,8 +19,8 @@ public class IcingaWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for(int i : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        for(int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
